5a89d173d803fb0eacf43dd0f1fc5d62e7000242
sumin <smkl1004@snu.ac.kr>
2025-03-25 20:10:10 +0900

    modify instagram hw

--- instagram.js
+++ instagram.js
@@ -1,5 +1,5 @@
 const storyModal = document.querySelector(".story-modal");
-const storyElements = document.querySelectorAll('.story-element');
+const storyElements = document.querySelectorAll(".story-element");
 
 storyElements.forEach( storyElement => {
    storyElement.addEventListener("click", () => {
@@ -7,26 +7,22 @@ storyElements.forEach( storyElement => {
    });
 });
 
-
 storyModal.addEventListener("click", (event) => {
       // TODO: storyModal 외부를 클릭했을 때, storyModal을 닫기
       if (event.target===storyModal) {
-      storyModal.style.display = "none";
-    }
+        storyModal.style.display = "none";
+      }
   }
 )
 
 const profile_container = document.querySelector(".profile-container")
 const profile_modal = document.querySelector("#profile-modal")
 
-
-
 profile_container.addEventListener(
    "mouseover", () => {
     profile_modal.style.display = "block";
     profile_modal.style.position = "absolute";
 });
-
 
 profile_container.addEventListener(
     "mouseout", () => {
@@ -37,23 +33,15 @@ const likeCount = document.getElementById('like-count');
 const blackHeart = document.getElementById('black-heart');
 const redHeart = document.getElementById('red-heart');
 
-
 blackHeart.addEventListener('click', () => {
-
-
   redHeart.style.display = "inline";
   blackHeart.style.display = "none";
 
-
   const count = likeCount.innerText;
-  
-  
-  
   likeCount.innerText = `${parseInt(count)+1}`;
   
   //TODO: 좋아요 버튼을 눌렀을 때, 알림창을 띄우기
   showAlert();
-
 })
 
 function showAlert() {
@@ -65,39 +53,33 @@ function showAlert() {
   setTimeout(() => {
     alertBox.classList.remove("show");
   }, 3000);
-
 }
 
 redHeart.addEventListener('click', () => {
-
   blackHeart.style.display = "inline";
   redHeart.style.display = "none";
 
-
   const count = likeCount.innerText;
-  
-
   likeCount.innerText = `${parseInt(count)-1}`;
-  
 })
 
 const commentsCreateForm = document.querySelector('.comments-create-form');
 const commentContainer = document.querySelector('.written-comments-container');
 const commentInput = document.querySelector('.comment');
 
-
 const commentsList = [];
 let commentId = 0;
 
 commentsCreateForm.addEventListener('submit', (e) => {
-
   e.preventDefault();
   const commentText = commentInput.value;
-  if (!commentText) return;
+  if (!commentText) {
+    return;
+  }
+
   commentsList.push(commentText);
-
-
 	commentId = commentsList.length;
+
   const commentNode = 
   `
     <div class="comment-wrapper">
@@ -112,31 +94,26 @@ commentsCreateForm.addEventListener('submit', (e) => {
     </div>
   `;
   
-  
   commentContainer.innerHTML = commentContainer.innerHTML + commentNode;
   commentInput.value = "";
-  
 })
 
-
-
 const deleteComment = (id) => {
-
   commentsList.splice(id, 1);
-
 
   commentContainer.innerHTML = commentsList.map((comment, index) => `
   <div class="comment-wrapper">
     <span class="comment">${comment}</span>
     <img id="${index}" class="comment-delete-icon" onclick="deleteComment(${index})" src="./images/close.png" alt="comment" />
   </div>`).join('');
-
 }
 
 //TODO: 인스타그램 로고 클릭 시 새로고침
 const logo=document.querySelector('.logo');
 logo.addEventListener('click', () => {
-  location.reload();});
+    location.reload();
+  }
+);
 
 //TODO: 아이콘 클릭 시 투명도 생성 
 const icons = document.querySelectorAll('.logo, .search, .icon-list, .feed-buttons');
@@ -157,7 +134,6 @@ const emojiModal=document.querySelector('.emoji-modal');
 const emojiSpans=document.querySelectorAll('.emoji');
 
 smile.addEventListener('click', () => {
-
   const btnRect = smile.getBoundingClientRect(); // 버튼 위치 정보 가져오기
 
   //버튼 위치 기준으로 이모티콘 모달 위치 정하기
@@ -166,7 +142,8 @@ smile.addEventListener('click', () => {
   
   if (emojiModal.style.display === "block") {
     emojiModal.style.display = "none";
-  } else {
+  }
+  else {
     emojiModal.style.display = "block";
   }
 });
@@ -176,4 +153,3 @@ emojiSpans.forEach((element) => {
     commentInput.value += element.innerHTML;
   });
 });
-
